32b001eeb027d1ccd6a22112717c7e432535ae2f
unknown <leos_87@hotmail.com>
2024-05-26 11:39:28 -0300

    Update angular version

--- src/app/components/increaser/increaser.component.ts
+++ src/app/components/increaser/increaser.component.ts
@@ -1,15 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-increaser',
   templateUrl: './increaser.component.html',
   styleUrls: ['./increaser.component.css'],
 })
-export class IncreaserComponent {
+export class IncreaserComponent implements OnInit {
   //@Input('value') progress: number = 50;
+  @Input() btnClass: string = 'btn-primary';
   @Input() progress: number = 50;
   @Output() valueOut: EventEmitter<number> = new EventEmitter();
 
+  ngOnInit() {
+    this.btnClass = `btn ${this.btnClass}`;
+  }
 
   changeValue(value: number) {
     if (this.progress >= 100 && value >= 0) {
@@ -22,5 +26,18 @@ export class IncreaserComponent {
     }
     this.valueOut.emit(this.progress + value);
     return (this.progress = this.progress + value);
+  }
+
+  onChange(newValue: number) {
+    if (newValue >= 100) {
+      this.progress = 100;
+    } else if (newValue <= 0) {
+      this.progress = 0;
+    } else this.progress = newValue;
+    this.valueOut.emit(this.progress);
+  }
+
+  isInvalidProgress(): boolean {
+    return this.progress > 100 || this.progress < 0;
   }
 }
