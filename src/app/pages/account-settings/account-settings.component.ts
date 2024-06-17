e0f357168e5a52f0967dc68bd2f27a9166af743c
unknown <leos_87@hotmail.com>
2024-06-17 11:13:22 -0300

    Update

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -7,13 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
   public linkTheme = document.querySelector('#theme');
+  public links: any;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+  }
 
   changeTheme(color: string) {
     const url = `./assets/css/colors/${color}.css`;
 
     this.linkTheme?.setAttribute('href', url);
     localStorage.setItem('theme', url);
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    this.links.forEach((element : any) => {
+      element.classList.remove('working');
+      const btnTheme = element.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if (btnThemeUrl === currentTheme) {
+        element.classList.add('working');
+      }
+    });
   }
 }
